1e8f4591e0223c73fbed58e0deeddf890ae6f93e
Salman A <salman.a@hatio.in>
2022-08-17 16:23:54 +0530

    Fix heading styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,18 +20,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal text-center font-extrabold text-gray-700 dark:text-gray-200">
-          Welcome to <span className="text-purple-300">T3 Blog</span>
+        <h1 className="text-5xl md:text-[4rem] xl:text-[5rem] leading-normal text-center font-extrabold text-gray-700 dark:text-gray-200">
+          Welcome to <span className="text-purple-300">T3 Blog</span>,
           {session && session.user && (
-            <span>
-              ,
-              <span className="text-red-500 dark:text-red-400">
-                {session.user.name || "User"}
-              </span>
+            <span className="text-red-500 dark:text-red-400">
+              {` ${session.user.name || "User"}`}
             </span>
           )}
         </h1>
-        <h2 className="text-4xl md:text-[4rem] leading-normal font-bold mb-6 text-amber-700 dark:text-amber-200">
+        <h2 className="text-4xl md:text-[3rem] xl:text-[4rem] leading-normal font-bold mb-6 text-amber-700 dark:text-amber-200">
           Posts
         </h2>
         <div className="flex flex-col items-center justify-center w-full">
@@ -68,7 +65,9 @@ const Home: NextPage = () => {
                 ))}
               </>
             ) : (
-              <Loader variant="bars" />
+              <div className="col-span-full">
+                <Loader variant="bars" />
+              </div>
             )}
           </div>
         </div>
